79b0559e91ae06b267eef81159f5c8fe0a215be0
DudaElias <42540760+DudaElias@users.noreply.github.com>
2019-04-16 09:35:24 -0300

    pos-fixo quase pronto, falta calculo, numero e paranteses

---
+++ Calculadora/Calculadora/Elemento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Elemento : IComparable<Elemento>
+    {
+        private char ele;
+        private int prefe;
+
+        public char Ele { get => ele; set => ele = value; }
+        public int Prefe { get => prefe; set => prefe = value; }
+
+        public Elemento(char el, int i)
+        {
+            Ele = el;
+            Prefe = i;
+        }
+
+        public int CompareTo(Elemento outro)
+        {
+            return 0;
+        }
+    }
+}
--- Calculadora/Calculadora/Form1.cs
+++ Calculadora/Calculadora/Form1.cs
@@ -15,12 +15,15 @@ namespace Calculadora
         public frmCal()
         {
             InitializeComponent();
-            pilhaElementos = new PilhaHerdaLista<char>();
-
+            pilhaElementos = new Elemento[20];
+            posFixo = new PilhaHerdaLista<char>();
+            elementosAEspera = new PilhaHerdaLista<char>();
         }
 
-        private PilhaHerdaLista<char> pilhaElementos;
+        private Elemento[] pilhaElementos;
         private PilhaHerdaLista<char> posFixo;
+        private PilhaHerdaLista<char> elementosAEspera;
+        private int qtd = 0;
         private void btnAbre_Click(object sender, EventArgs e)
         {
 
@@ -29,13 +32,16 @@ namespace Calculadora
                 if (Convert.ToChar((sender as Button).Text) == 'C')
                 {
                     txtResultado.Text = txtResultado.Text.Remove(txtResultado.TextLength - 1);
-                    pilhaElementos.Desempilhar();
+                    pilhaElementos[qtd] = null;
                 }
                 else
                 {
                     txtResultado.Text += Convert.ToChar((sender as Button).Text);
-                    pilhaElementos.Empilhar(Convert.ToChar((sender as Button).Text));
+                    Elemento ele = new Elemento(Convert.ToChar((sender as Button).Text),DecidirPreferencia((sender as Button).Text));
+                    pilhaElementos[qtd] = ele;
+                    qtd++;
                 }
+
             }
             else
             {
@@ -43,46 +49,49 @@ namespace Calculadora
             }
         }
 
-        public void ConverterParaPosFixa()
+        public int DecidirPreferencia(string e)
         {
             
-            switch(pilhaElementos.OTopo().ToString())
+            switch(e)
             {
                 case "1":
-
-                    break;
+                    return 4;
                 case "2":
-                    break;
+                    return 4;
                 case "3":
-                    break;
+                    return 4;
                 case "4":
-                    break;
+                    return 4;
                 case "5":
-                    break;
+                    return 4;
                 case "6":
-                    break;
+                    return 4;
                 case "7":
-                    break;
+                    return 4;
                 case "8":
-                    break;
+                    return 4;
                 case "9":
-                    break;
+                    return 4;
                 case "0":
-                    break;
-                case ".":
-                    break;
+                    return 4;
                 case "/":
-                    break;
+                    return 2;
                 case "*":
-                    break;
+                    return 2;
                 case "+":
-                    break;
+                    return 3;
                 case "-":
-                    break;
+                    return 3;
                 case "^":
-                    break;
+                    return 1;
 
             }
+            return 0;
+        }
+
+        public void ConverterParaPosFixa()
+        {
+
         }
 
         private void frmCal_Load(object sender, EventArgs e)
@@ -92,7 +101,57 @@ namespace Calculadora
 
         private void btnIgual_Click(object sender, EventArgs e)
         {
-
+            int qtdNum = 0;
+            for(int i = 0;  qtd != 0; i++)
+            {
+                if (pilhaElementos[i].Prefe == 4 && qtdNum != 2) // se é número
+                {
+                    posFixo.Empilhar(pilhaElementos[i].Ele);
+                    pilhaElementos[i] = null;
+                    qtdNum++;
+                    qtd--;
+                }
+                else if(qtdNum != 2) // caso não tenha dois números ainda
+                {
+                    elementosAEspera.Empilhar(pilhaElementos[i].Ele);
+                    pilhaElementos[i] = null;
+                    qtd--;
+                }
+                else if(DecidirPreferencia(Convert.ToString(elementosAEspera.OTopo())) > pilhaElementos[i].Prefe)
+                {
+                    if (qtdNum == 2)
+                    {
+                        posFixo.Empilhar(pilhaElementos[i].Ele);
+                        pilhaElementos[i] = null;
+                        qtd--;
+                        qtdNum = 0;
+                    }
+                }
+                else if(DecidirPreferencia(Convert.ToString(elementosAEspera.OTopo())) <= pilhaElementos[i].Prefe)
+                {
+                    if(qtdNum == 2)
+                    {
+                        posFixo.Empilhar(elementosAEspera.OTopo());
+                        qtd--;
+                    }
+                }
+            }
+            if(!elementosAEspera.EstaVazia())
+            {
+                elementosAEspera.Atual = elementosAEspera.Primeiro;
+                while(!elementosAEspera.EstaVazia())
+                {
+                    posFixo.Empilhar(elementosAEspera.Atual.Info);
+                    elementosAEspera.Desempilhar();
+                }
+            }
+            posFixo.Inverter();
+            posFixo.Atual = posFixo.Primeiro;
+            for (int i = 1; i <= posFixo.Tamanho(); i++ )
+            {
+                lblPos.Text += posFixo.Atual.Info;
+                posFixo.Atual = posFixo.Atual.Prox;
+            }
         }
     }
 }
